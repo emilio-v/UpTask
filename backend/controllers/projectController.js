c978f2ef07b1d7354d180ad0e392bb9c6ebf6565
emilio-v <emilio_785@hotmail.com>
2022-03-13 00:06:10 -0600

    update project

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -43,7 +43,40 @@ const getProject = async (req, res) => {
   res.json(project);
 };
 
-const editProject = async (req, res) => {};
+const editProject = async (req, res) => {
+  const { id } = req.params;
+
+  const checkId = mongoose.Types.ObjectId.isValid(id);
+
+  if (!checkId) {
+    const error = new Error("Project not found");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  const project = await Project.findById(id);
+
+  if (!project) {
+    const error = new Error("Project not found");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Invalid action");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  project.name = req.body.name || project.name;
+  project.description = req.body.description || project.description;
+  project.deliveryDate = req.body.deliveryDate || project.deliveryDate;
+  project.client = req.body.client || project.client;
+
+  try {
+    const savedProject = await project.save();
+    res.json(savedProject);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const deleteProject = async (req, res) => {};
 
