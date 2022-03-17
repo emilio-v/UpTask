5c62b9bc6c736d48cf7ea360e07bf1f095925425
emilio-v <emilio_785@hotmail.com>
2022-03-16 22:44:38 -0600

    created routing and controllers from task model

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 // we're using express.json to get the data as a json file
@@ -14,6 +15,7 @@ connectDB();
 // Routing
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
+app.use("/api/task", taskRoutes);
 
 const PORT = process.env.PORT || 4000;
 
