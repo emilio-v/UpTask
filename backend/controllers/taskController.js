05d26c189d2c44216916c4ce0ee59de695dbbf5e
emilio-v <emilio_785@hotmail.com>
2022-03-23 23:10:41 -0600

    delete task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -89,7 +89,34 @@ const updateTask = async (req, res) => {
   }
 };
 
-const deleteTask = async (req, res) => {};
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  const checkId = mongoose.Types.ObjectId.isValid(id);
+
+  if (!checkId) {
+    const error = new Error("Task not found");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  const task = await Task.findById(id).populate("project");
+
+  if (!task) {
+    const error = new Error("Task not found");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (task.project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Invalid action");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    await task.deleteOne();
+    res.json({ msg: "Deleted task" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const changeState = async (req, res) => {};
 
