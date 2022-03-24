b7455ca31f23390d5e91a27da93698fff8b8bf16
emilio-v <emilio_785@hotmail.com>
2022-03-23 23:01:03 -0600

    update task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -53,7 +53,41 @@ const getTask = async (req, res) => {
   res.json(task);
 };
 
-const updateTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const checkId = mongoose.Types.ObjectId.isValid(id);
+
+  if (!checkId) {
+    const error = new Error("Task not found");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  // "Populate" merge the data from Task and Project. This is possible since we're using a ref to Project model in the project column
+  // of the Task model. For more information you can see https://mongoosejs.com/docs/populate.html
+  const task = await Task.findById(id).populate("project");
+
+  if (!task) {
+    const error = new Error("Task not found");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (task.project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Invalid action");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  task.name = req.body.name || task.name;
+  task.description = req.body.description || task.description;
+  task.priority = req.body.priority || task.priority;
+  task.deliveryDate = req.body.deliveryDate || task.deliveryDate;
+
+  try {
+    const savedTask = await task.save();
+    res.json(savedTask);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const deleteTask = async (req, res) => {};
 
