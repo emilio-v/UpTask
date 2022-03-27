0ca9afda899a893da2b9a32620f6fbb149be88ea
emilio-v <emilio_785@hotmail.com>
2022-03-27 01:02:54 -0600

    created sign up screen

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>Sign Up</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Create your account and manage your{" "}
+        <span className="text-slate-700">projects</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Name"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="confirm-password"
+          >
+            Confirm Password
+          </label>
+          <input
+            id="confirm-password"
+            type="password"
+            placeholder="Confirm your Password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Create account"
+          className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/"
+        >
+          Already have an account? Login
+        </Link>
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/forgot-password"
+        >
+          Forgot your password?
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default SignUp;
