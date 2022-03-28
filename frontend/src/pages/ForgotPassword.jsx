df482d04166d138ff90e3887848a5095a39e7048
emilio-v <emilio_785@hotmail.com>
2022-03-27 22:26:19 -0600

    created forgot password screen

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,5 +1,51 @@
+import { Link } from "react-router-dom";
+
 const ForgotPassword = () => {
-  return <div>Forgot Password</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Recover your access and don't lose your{" "}
+        <span className="text-slate-700">projects</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Send instructions"
+          className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/"
+        >
+          Already have an account? Login
+        </Link>
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/sign-up"
+        >
+          Don't have an account yet? Register now
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default ForgotPassword;
