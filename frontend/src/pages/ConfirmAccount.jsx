ece6d979d609f52e84fb8cf3982b2de7def42c4c
emilio-v <emilio_785@hotmail.com>
2022-03-27 22:42:41 -0600

    create reset password and confirm account screen

--- frontend/src/pages/ConfirmAccount.jsx
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -1,5 +1,12 @@
 const ConfirmAccount = () => {
-  return <div>Confirm Account</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Confirm your <span className="text-slate-700">account</span> and start
+        creating your <span className="text-slate-700">projects</span>
+      </h1>
+    </>
+  );
 };
 
 export default ConfirmAccount;
--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -1,5 +1,34 @@
 const ResetPassword = () => {
-  return <div>Reset Password</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Reset your <span className="text-slate-700">password</span> and don't
+        lose access to your <span className="text-slate-700">projects</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+          >
+            New Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter your new password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Save new password"
+          className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
+  );
 };
 
 export default ResetPassword;
