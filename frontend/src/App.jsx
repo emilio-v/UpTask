51533a6353ffc8b29d071062876cbbbbc42da7e6
emilio-v <emilio_785@hotmail.com>
2022-05-02 00:26:42 -0500

    created Projects Context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthProvider";
+import { ProjectsProvider } from "./context/ProjectsProvider";
 
 import AuthLayout from "./layouts/AuthLayout";
 import Login from "./pages/Login";
@@ -17,20 +18,25 @@ const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-            <Route path="sign-up" element={<SignUp />} />
-            <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="forgot-password/:token" element={<ResetPassword />} />
-            <Route path="confirm/:token" element={<ConfirmAccount />} />
-          </Route>
+        <ProjectsProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="sign-up" element={<SignUp />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route
+                path="forgot-password/:token"
+                element={<ResetPassword />}
+              />
+              <Route path="confirm/:token" element={<ConfirmAccount />} />
+            </Route>
 
-          <Route path="/projects" element={<ProtectedRoute />}>
-            <Route index element={<Projects />} />
-            <Route path="new-project" element={<NewProject />} />
-          </Route>
-        </Routes>
+            <Route path="/projects" element={<ProtectedRoute />}>
+              <Route index element={<Projects />} />
+              <Route path="new-project" element={<NewProject />} />
+            </Route>
+          </Routes>
+        </ProjectsProvider>
       </AuthProvider>
     </BrowserRouter>
   );
---
+++ frontend/src/context/ProjectsProvider.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect, createContext } from "react";
+import axiosClient from "../config/axiosClient";
+
+const ProjectsContext = createContext();
+
+const ProjectsProvider = ({ children }) => {
+  const [projects, setProjects] = useState([]);
+  return (
+    <ProjectsContext.Provider value={{ projects }}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
+
+export { ProjectsProvider };
+
+export default ProjectsContext;
