aeebd569ac30ac9d44e807a5df18f14bbbc4583d
emilio-v <emilio_785@hotmail.com>
2022-05-01 23:46:16 -0500

    add new project route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import ConfirmAccount from "./pages/ConfirmAccount";
 
 import ProtectedRoute from "./layouts/ProtectedRoute";
 import Projects from "./pages/Projects";
+import NewProject from "./pages/NewProject";
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
 
           <Route path="/projects" element={<ProtectedRoute />}>
             <Route index element={<Projects />} />
+            <Route path="new-project" element={<NewProject />} />
           </Route>
         </Routes>
       </AuthProvider>
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import useAuth from "../hooks/useAuth";
+
 const SideBar = () => {
+  const { auth } = useAuth();
+
   return (
     <aside className="md:w-80 lg:w-96 px-5 py-10">
-      <p className="text-xl font-bold">Hola Emilio!</p>
+      <p className="text-xl font-bold">Hi {auth.name}!</p>
 
       <Link
-        to="/new-project"
+        to="new-project"
         className="bg-sky-600 w-full p-3 text-white uppercase font-bold block mt-5 text-center rounded-lg"
       >
         New Project
